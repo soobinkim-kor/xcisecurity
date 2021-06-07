7fa0b8f883f366f8a090c18dc8cfe6206db75dbc
COM <COM@DESKTOP-L2F7LSG.mshome.net>
2021-06-07 14:04:41 +0900

    commit~

--- src/main/java/net/developia/sec/HomeController.java
+++ src/main/java/net/developia/sec/HomeController.java
@@ -6,7 +6,6 @@ import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 /**
  * Handles requests for the application home page.
  */
-@Controller
+//@Controller
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
